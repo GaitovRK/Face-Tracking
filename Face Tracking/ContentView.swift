e00a1d982ae88d6a05664d5d62eeedbb977ca440
Rashid <rashidgaitov@gmail.com>
2023-11-27 12:55:12 +0300

    Initial Commit

---
+++ Face Tracking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Face Tracking
+//
+//  Created by Rashid Gaitov on 27.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Face Tracking/Face_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Face_TrackingApp.swift
+//  Face Tracking
+//
+//  Created by Rashid Gaitov on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Face_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
